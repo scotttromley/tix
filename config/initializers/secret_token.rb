512beabbda820e58167d35e4362619ee150ec254
Scott Tromley <scott@tromley.ca>
2012-09-20 19:06:21 -0400

    init

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Tix::Application.config.secret_token = '42066f5e5001209fe591588e8f385451fc43c9cbb47e82df4f6616f4983f4925ac6589ed57bf135a3dfe46fde406cd619c772f2138567286b3464e75e7eaf17d'
